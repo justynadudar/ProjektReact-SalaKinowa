a181c7eb48a194b8d65a77bfd0ba8c5c9de20058
Justyna Dudar <justyna.dudar1@gmail.com>
2021-12-03 14:12:36 +0100

    post method with redux added

--- app/src/FilmList.js
+++ app/src/FilmList.js
@@ -3,93 +3,80 @@ import Showing from "./Showing";
 import AddFilm from "./AddFilm.js";
 import Home from "./Home.js";
 import React from "react";
+import { connect } from "react-redux";
+import { newFilm } from "./actions";
 
 class FilmList extends React.Component {
     constructor(props) {
         super(props);
-        this.addFilm = this.addFilm.bind(this);
-        this.state = {
-            films: [
-                {
-                    title: "Piraci z Karaibów: Klątwa Czarnej Perły",
-                    duration: 143,
-                },
-                {
-                    title: "Wyspa tajemnic",
-                    duration: 138,
-                },
-                {
-                    title: "Człowiek demolka",
-                    duration: 113,
-                },
-            ],
-        };
     }
 
-    addFilm(text) {
-        const { dane } = this.state;
-        return this.setState([...dane, { title: text }]);
-    }
 
-    // componentDidMount() {
-    // fetch(`http://localhost:3001/orders`, {
-    //         method: 'post',
-    //       headers:{
-    //           'Content-Type': 'application/json',
-    //           'Accept': 'application/json'
+    add(title){
+         const { films } = this.props;
+         const id = this.props.films.id;
+        const sth = {
+                id: id,
+                title: title,
+                duration: 130,
+                imgUrl: "https://cdn.pixabay.com/photo/2016/09/30/11/33/vintage-1705056_960_720.jpg",
+                dates: [],
+                cinemaHallId: 2
+            }
+        this.props.newFilm(sth);
+    }
+    
+
+    // async add() {
+    //     const sth = {
+    //         id: 6,
+    //         title: "John Smith",
+    //         duration: 110,
+    //         imgUrl: "https://cdn.pixabay.com/photo/2016/09/30/20/22/vintage-1706242_960_720.jpg",
+    //         dates: [],
+    //         cinemaHallId: null,
+    //     };
+    //     console.log(sth);
+    //     await fetch("http://localhost:3001/orders", {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             Accept: "application/json",
     //         },
-    //       body: JSON.stringify({
-    //           id: 3,
-    //           title: "John Smith",
-    //           duration: 110,
-    //           imgUrl: "https://cdn.pixabay.com/photo/2016/09/30/20/22/vintage-1706242_960_720.jpg",
-    //           dates:  [],
-    //           cinemaHallId: null
-    //       })
-    //   }).then(response => response.json())
-    //   .then(data => this.setState({dane: data}));
-
+    //         body: JSON.stringify(sth),
+    //     })
+    //         .then((response) => response.json())
+    //         .then(() => {
+    //             alert("Dodano użytkownika");
+    //         })
+    //         .catch((error) => {
+    //             console.error(error);
+    //         });
     // }
 
-    async add() {
-        const sth = {
-            id: 4,
-            title: "John Smith",
-            duration: 110,
-            imgUrl: "https://cdn.pixabay.com/photo/2016/09/30/20/22/vintage-1706242_960_720.jpg",
-            dates: [],
-            cinemaHallId: null,
-        };
-        console.log(sth);
-        await fetch("http://localhost:3001/orders", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-            },
-            body: JSON.stringify(sth),
-        })
-            .then((response) => response.json())
-            .then(() => {
-                alert("Dodano użytkownika");
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }
-
     render() {
-        const { dane } = this.state;
+        
         return (
             <div>
                 <Home />
                 <ul>
                     {/* {dane.map((element) =>  <Showing film={element}/>)} */}
-                    <AddFilm handleClick={() => this.add()} />
+                    <AddFilm handleClick={(title) => this.add(title)} />
+                    <AddFilm handleClick={(title) => this.add(title)} />
                 </ul>
             </div>
         );
     }
 }
 
-export default FilmList;
+const mapStateToProps = (state) => {
+    return { ...state };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        newFilm: (new_film) => dispatch(newFilm({data:new_film})),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(FilmList);
