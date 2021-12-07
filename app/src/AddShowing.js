79d5fd0318a98c89c9773b8635a69bf6fc85df21
TheRealSlimStefan <19smarcins98@gmail.com>
2021-12-07 04:26:44 +0100

    working all data validation

--- app/src/AddShowing.js
+++ app/src/AddShowing.js
@@ -3,162 +3,281 @@ import "./style/AddShowing.css";
 import { Link, Redirect } from "react-router-dom";
 import { BsArrowLeftShort } from "react-icons/bs";
 import PropTypes from "prop-types";
+import Error from "./Error";
 
 class AddShowing extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      textInput: "",
-      cinemaHallInput: "",
-      dateInput: this.props.location.state.todayDate,
-      timeInput: "",
-      status: false,
+    constructor(props) {
+        super(props);
+        this.state = {
+            textInput: "",
+            cinemaHallInput: "",
+            dateInput: this.props.location.state.todayDate,
+            timeInput: "",
+            status: false,
+            emptyTitleField: false,
+            emptyCinemaHallField: false,
+            emptyDateField: false,
+            emptyTimeField: false,
+            invalidFilmName: false,
+            invalidCinemaHallNumber: false,
+            invalidDate: false,
+            invalidTime: false,
+        };
+    }
+    componentDidMount() {
+        this.props.getData();
+    }
+
+    changeCinemaHall = (e) => {
+        this.setState({
+            cinemaHallInput: e.target.value,
+            emptyCinemaHall: false,
+        });
     };
-  }
-  componentDidMount() {
-    this.props.getData();
-  }
 
-  changecinemaHall = (e) => {
-    this.setState({
-      cinemaHallInput: e.target.value,
-    });
-  };
-
-  changeText = (e) => {
-    this.setState({
-      textInput: e.target.value,
-    });
-  };
-  changeDate = (e) => {
-    this.setState({
-      dateInput: e.target.value,
-    });
-  };
-  changeTime = (e) => {
-    this.setState({
-      timeInput: e.target.value,
-    });
-  };
-
-  addShowingToShowingList() {
-    const showingId = Math.floor(Math.random() * 99999) + 1;
-    const updatedElement = this.props.films.data.find(
-      (element) => element.title === this.state.textInput
-    );
-    const cinemaHall = this.props.films.cinemaHalls.find(
-      (element) => element.hallId === Number(this.state.cinemaHallInput)
-    );
-    const newShowing = {
-      id: updatedElement.id,
-      title: updatedElement.title,
-      duration: updatedElement.duration,
-      imgUrl: updatedElement.imgUrl,
-      showings: [
-        ...updatedElement.showings,
-        {
-          showingId: showingId,
-          date: this.state.dateInput,
-          hour: this.state.timeInput,
-          occupiedSeats: [],
-          cinemaHall: {
-            hallId: Number(this.state.cinemaHallInput),
-            capacity: cinemaHall.capacity,
-            body: cinemaHall.body,
-          },
-          numberOfSeatsSold: 0,
-          numberOfAvaibleSeats: cinemaHall.capacity,
-        },
-      ],
+    changeText = (e) => {
+        this.setState({
+            textInput: e.target.value,
+            emptyTitleField: false,
+            invalidFilmName: false,
+        });
     };
-    this.props.addShowing(newShowing, updatedElement.id);
-    this.setState({ status: true });
-    this.props.getData();
-    this.props.showShowingsOfThatDay();
-  }
+    changeDate = (e) => {
+        this.setState({
+            dateInput: e.target.value,
+            emptyDateField: false,
+        });
+    };
+    changeTime = (e) => {
+        this.setState({
+            timeInput: e.target.value,
+            emptyTimeField: false,
+        });
+    };
 
-  render() {
-    const { films } = this.props;
-    const { status, dateInput, timeInput, textInput, cinemaHallInput } =
-      this.state;
-    if (status === true) return <Redirect to="/showings" />;
-    return (
-      <div className="AddShowing">
-        <div>
-          <Link to="/showings">
-            <BsArrowLeftShort />
-          </Link>
-          <h2>Dodaj seans:</h2>
-          <label htmlFor="title">Film:</label>
-          <input
-            type="text"
-            list="films"
-            name="title"
-            value={textInput}
-            onChange={this.changeText}
-          />
-          <datalist id="films">
-            {films.loaded
-              ? films.data.map((film) => (
-                  <option key={Math.random()}>{film.title}</option>
-                ))
-              : null}
-          </datalist>
-          <label htmlFor="cinemaHall">Sala:</label>
-          <input
-            type="text"
-            list="cinemaHalls"
-            name="hallId"
-            value={cinemaHallInput}
-            onChange={this.changecinemaHall}
-          />
-          <datalist id="cinemaHalls">
-            {films.loaded
-              ? films.cinemaHalls.map((cinemaHall) => (
-                  <option key={Math.random()}>{cinemaHall.hallId}</option>
-                ))
-              : null}
-          </datalist>
-          <label htmlFor="date">Data:</label>
-          <input
-            type="date"
-            name="date"
-            value={dateInput}
-            onChange={this.changeDate}
-          />
-          <label htmlFor="time">Godzina:</label>
-          <input
-            type="time"
-            name="hour"
-            value={timeInput}
-            onChange={this.changeTime}
-          />
-          <button onClick={() => this.addShowingToShowingList()}>Dodaj</button>
-        </div>
-      </div>
-    );
-  }
+    addShowingToShowingList() {
+        const { dateInput, timeInput, textInput, cinemaHallInput } = this.state;
+
+        this.setState({
+            emptyTitleField: false,
+            emptyCinemaHallField: false,
+            emptyDateField: false,
+            emptyTimeField: false,
+            invalidFilmName: false,
+            invalidCinemaHallNumber: false,
+            invalidDate: false,
+            invalidTime: false,
+        });
+
+        let invalidFilmName = false;
+        let today = new Date();
+        let stringTodayTime =
+            ("0" + today.getHours()).slice(-2) + ":" + today.getMinutes();
+
+        this.props.films.data.forEach((film) => {
+            if (textInput !== film.title) invalidFilmName = true;
+        });
+
+        if (textInput.length === 0) {
+            this.setState({
+                emptyTitleField: true,
+            });
+        } else if (invalidFilmName) {
+            this.setState({
+                invalidFilmName: true,
+            });
+        } else if (cinemaHallInput.length === 0) {
+            this.setState({
+                emptyCinemaHallField: true,
+            });
+        } else if (cinemaHallInput <= 0 || cinemaHallInput >= 6) {
+            this.setState({
+                invalidCinemaHallNumber: true,
+            });
+        } else if (dateInput.length === 0) {
+            this.setState({
+                emptyDateField: true,
+            });
+        } else if (
+            Date.parse(dateInput) <
+            Date.parse(this.props.location.state.todayDate)
+        ) {
+            this.setState({
+                invalidDate: true,
+            });
+        } else if (timeInput.length === 0) {
+            this.setState({
+                emptyTimeField: true,
+            });
+        } else if (timeInput <= stringTodayTime) {
+            this.setState({
+                invalidTime: true,
+            });
+        } else {
+            const showingId = Math.floor(Math.random() * 99999) + 1;
+            const updatedElement = this.props.films.data.find(
+                (element) => element.title === this.state.textInput
+            );
+            const cinemaHall = this.props.films.cinemaHalls.find(
+                (element) =>
+                    element.hallId === Number(this.state.cinemaHallInput)
+            );
+            const newShowing = {
+                id: updatedElement.id,
+                title: updatedElement.title,
+                duration: updatedElement.duration,
+                imgUrl: updatedElement.imgUrl,
+                showings: [
+                    ...updatedElement.showings,
+                    {
+                        showingId: showingId,
+                        date: this.state.dateInput,
+                        hour: this.state.timeInput,
+                        occupiedSeats: [],
+                        cinemaHall: {
+                            hallId: Number(this.state.cinemaHallInput),
+                            capacity: cinemaHall.capacity,
+                            body: cinemaHall.body,
+                        },
+                        numberOfSeatsSold: 0,
+                        numberOfAvaibleSeats: cinemaHall.capacity,
+                    },
+                ],
+            };
+            this.props.addShowing(newShowing, updatedElement.id);
+            this.setState({ status: true });
+            this.props.getData();
+            this.props.showShowingsOfThatDay();
+        }
+    }
+
+    render() {
+        const { films } = this.props;
+        const {
+            status,
+            dateInput,
+            timeInput,
+            textInput,
+            cinemaHallInput,
+            emptyTitleField,
+            emptyCinemaHallField,
+            emptyDateField,
+            emptyTimeField,
+            invalidFilmName,
+            invalidCinemaHallNumber,
+            invalidDate,
+            invalidTime,
+        } = this.state;
+        if (status === true) return <Redirect to="/showings" />;
+        return (
+            <div className="AddShowing">
+                <div>
+                    <Link to="/showings">
+                        <BsArrowLeftShort />
+                    </Link>
+                    <h2>Dodaj seans:</h2>
+                    <label htmlFor="title">Film:</label>
+                    <input
+                        type="text"
+                        list="films"
+                        name="title"
+                        value={textInput}
+                        onChange={this.changeText}
+                    />
+                    <datalist id="films">
+                        {films.loaded
+                            ? films.data.map((film) => (
+                                  <option key={Math.random()}>
+                                      {film.title}
+                                  </option>
+                              ))
+                            : null}
+                    </datalist>
+                    <label htmlFor="cinemaHall">Sala:</label>
+                    <input
+                        type="number"
+                        list="cinemaHalls"
+                        name="hallId"
+                        value={cinemaHallInput}
+                        onChange={this.changeCinemaHall}
+                    />
+                    <datalist id="cinemaHalls">
+                        {films.loaded
+                            ? films.cinemaHalls.map((cinemaHall) => (
+                                  <option key={Math.random()}>
+                                      {cinemaHall.hallId}
+                                  </option>
+                              ))
+                            : null}
+                    </datalist>
+                    <label htmlFor="date">Data:</label>
+                    <input
+                        type="date"
+                        name="date"
+                        value={dateInput}
+                        onChange={this.changeDate}
+                    />
+                    <label htmlFor="time">Godzina:</label>
+                    <input
+                        type="time"
+                        name="hour"
+                        value={timeInput}
+                        onChange={this.changeTime}
+                    />
+                    <Error
+                        error={emptyTitleField}
+                        info="Pole tytuł nie może być puste!"
+                    />
+                    <Error
+                        error={invalidFilmName}
+                        info="Nie ma takiego filmu!"
+                    />
+                    <Error
+                        error={emptyCinemaHallField}
+                        info="Pole sala nie może być puste!"
+                    />
+                    <Error
+                        error={invalidCinemaHallNumber}
+                        info="Nie ma takiej sali!"
+                    />
+                    <Error
+                        error={emptyDateField}
+                        info="Pole data nie może być puste!"
+                    />
+                    <Error error={invalidDate} info="Niepoprawna data!" />
+                    <Error
+                        error={emptyTimeField}
+                        info="Pole godzina nie może być puste!"
+                    />
+                    <Error error={invalidTime} info="Niepoprawna godzina!" />
+                    <button onClick={() => this.addShowingToShowingList()}>
+                        Dodaj
+                    </button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default AddShowing;
 
 AddShowing.propTypes = {
-  addShowing: PropTypes.func.isRequired,
-  getData: PropTypes.func.isRequired,
-  films: PropTypes.shape({
-    error: PropTypes.string,
-    data: PropTypes.array,
-    loaded: PropTypes.bool,
-  }).isRequired,
-  location: PropTypes.shape({
-    pathname: PropTypes.string,
-    search: PropTypes.string,
-    hash: PropTypes.string,
-    key: PropTypes.string,
-    state: PropTypes.shape({
-      modal: PropTypes.bool,
-      todayDate: PropTypes.string.isRequired,
+    addShowing: PropTypes.func.isRequired,
+    getData: PropTypes.func.isRequired,
+    films: PropTypes.shape({
+        error: PropTypes.string,
+        data: PropTypes.array,
+        loaded: PropTypes.bool,
     }).isRequired,
-  }).isRequired,
-  showShowingsOfThatDay: PropTypes.func.isRequired,
+    location: PropTypes.shape({
+        pathname: PropTypes.string,
+        search: PropTypes.string,
+        hash: PropTypes.string,
+        key: PropTypes.string,
+        state: PropTypes.shape({
+            modal: PropTypes.bool,
+            todayDate: PropTypes.string.isRequired,
+        }).isRequired,
+    }).isRequired,
+    showShowingsOfThatDay: PropTypes.func.isRequired,
 };
--- app/src/BuyTicket.js
+++ app/src/BuyTicket.js
@@ -2,156 +2,200 @@ import React from "react";
 import { Link, Redirect } from "react-router-dom";
 import "./style/BuyTicket.css";
 import { BsArrowLeftShort } from "react-icons/bs";
+import Error from "./Error";
 // import PropTypes from "prop-types";
 
 class BuyTicket extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cinemaHall: [],
-      newOccupiedSeats: [],
-      cinemaHallId: 0,
-      updatedFilm: {},
-      updatedShowing: {},
-      showingId: Number(this.props.location.state.showingId),
-      numberOfSeatsSold: 0,
-      numberOfAvaibleSeats: 40,
-      status: false,
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            cinemaHall: [],
+            newOccupiedSeats: [],
+            cinemaHallId: 0,
+            updatedFilm: {},
+            updatedShowing: {},
+            showingId: Number(this.props.location.state.showingId),
+            numberOfSeatsSold: 0,
+            numberOfAvaibleSeats: 40,
+            status: false,
+            chooseError: false,
+        };
+    }
 
-  componentDidMount() {
-    this.props.getData();
-    this.props.showShowingsOfThatDay();
-    const tmpFilm = this.props.films.data.find(
-      (element) => element.id === this.props.location.state.filmId
-    );
-    this.setState({ updatedFilm: tmpFilm });
+    componentDidMount() {
+        this.props.getData();
+        this.props.showShowingsOfThatDay();
+        const tmpFilm = this.props.films.data.find(
+            (element) => element.id === this.props.location.state.filmId
+        );
+        this.setState({ updatedFilm: tmpFilm });
 
-    const tmpShowing = tmpFilm.showings.find(
-      (element) => element.showingId === this.state.showingId
-    );
-    const tmpHall = this.props.films.cinemaHalls.find(
-      (element) => element.hallId === tmpShowing.cinemaHall.hallId
-    );
-    this.setState({
-      cinemaHallId: tmpHall.hallId,
-    });
-
-    const tmpCinemaHall = tmpHall.body;
-
-    console.log(tmpCinemaHall);
-    tmpShowing.occupiedSeats.forEach((place) => {
-      tmpCinemaHall.forEach((row, rowId, rowTab) => {
-        if (place.row === rowId) {
-          rowTab[rowId].forEach((seat, seatId, seatTab) => {
-            if (place.place === seat.id) seatTab[seatId].id = "X";
-            return seat;
-          });
-        }
-        return row;
-      });
-      return place;
-    });
-
-    this.setState({
-      updatedShowing: tmpShowing,
-      numberOfSeatsSold: tmpShowing.numberOfSeatsSold,
-      numberOfAvaibleSeats: tmpShowing.numberOfAvaibleSeats,
-    });
-    this.setState({ cinemaHall: tmpCinemaHall });
-  }
-
-  choosePlace(row1, place1) {
-    //przypisuje do zmiennej pomocniczej sale kinowe ze stanu, następnie przechodzę po wierszach, następnie miejscach w poszukiwaniu klikniętego miejsca i toggluje jego stan isActive
-    const tmpCinemaHall = this.state.cinemaHall;
-    tmpCinemaHall.forEach((row, rowId, rowTab) => {
-      if (row1 === rowId) {
-        rowTab[rowId].forEach((seat, seatId, seatTab) => {
-          if (place1.id === seat.id)
-            seatTab[seatId].isActive = !seatTab[seatId].isActive;
-          return seat;
+        const tmpShowing = tmpFilm.showings.find(
+            (element) => element.showingId === this.state.showingId
+        );
+        const tmpHall = this.props.films.cinemaHalls.find(
+            (element) => element.hallId === tmpShowing.cinemaHall.hallId
+        );
+        this.setState({
+            cinemaHallId: tmpHall.hallId,
         });
-      }
-      return row;
-    });
 
-    this.setState({ cinemaHall: tmpCinemaHall });
-  }
+        const tmpCinemaHall = tmpHall.body;
 
-  buyTicket() {
-    const tmpupdatedShowing = this.state.updatedShowing;
+        console.log(tmpCinemaHall);
+        tmpShowing.occupiedSeats.forEach((place) => {
+            tmpCinemaHall.forEach((row, rowId, rowTab) => {
+                if (place.row === rowId) {
+                    rowTab[rowId].forEach((seat, seatId, seatTab) => {
+                        if (place.place === seat.id) seatTab[seatId].id = "X";
+                        return seat;
+                    });
+                }
+                return row;
+            });
+            return place;
+        });
 
-    //przechodze po wszystkich zaznaczonych miejscach i dodaje je w zajętych miejscach danego seansu
-    const tmpCinemaHall = this.state.cinemaHall;
-    tmpCinemaHall.forEach((row, rowId, rowTab) => {
-      rowTab[rowId].forEach((seat, seatId, seatTab) => {
-        if (seat.isActive === true) {
-          tmpupdatedShowing.occupiedSeats = [
-            ...tmpupdatedShowing.occupiedSeats,
-            { row: rowId, place: seat.id },
-          ];
+        this.setState({
+            updatedShowing: tmpShowing,
+            numberOfSeatsSold: tmpShowing.numberOfSeatsSold,
+            numberOfAvaibleSeats: tmpShowing.numberOfAvaibleSeats,
+        });
+        this.setState({ cinemaHall: tmpCinemaHall });
+    }
+
+    choosePlace(row1, place1) {
+        //przypisuje do zmiennej pomocniczej sale kinowe ze stanu, następnie przechodzę po wierszach, następnie miejscach w poszukiwaniu klikniętego miejsca i toggluje jego stan isActive
+        const tmpCinemaHall = this.state.cinemaHall;
+        tmpCinemaHall.forEach((row, rowId, rowTab) => {
+            if (row1 === rowId) {
+                rowTab[rowId].forEach((seat, seatId, seatTab) => {
+                    if (place1.id === seat.id)
+                        seatTab[seatId].isActive = !seatTab[seatId].isActive;
+                    return seat;
+                });
+            }
+            return row;
+        });
+
+        this.setState({ cinemaHall: tmpCinemaHall });
+    }
+
+    buyTicket() {
+        const tmpupdatedShowing = this.state.updatedShowing;
+        let chooseError = false;
+        this.setState({ chooseError: false });
+        let counter = 0;
+
+        const tmpCinemaHall = this.state.cinemaHall;
+        tmpCinemaHall.forEach((row, rowId, rowTab) => {
+            rowTab[rowId].forEach((seat, seatId, seatTab) => {
+                if (seat.isActive !== true) {
+                    counter++;
+                }
+            });
+        });
+
+        if (counter === tmpupdatedShowing.cinemaHall.capacity) {
+            chooseError = true;
         }
-        return seat;
-      });
-      return row;
-    });
 
-    this.state.updatedFilm.showings.forEach((showing, showId, showTab) => {
-      if (showing.showingId === this.state.showingId)
-        showTab[showId] = tmpupdatedShowing;
-      return showing;
-    });
+        if (chooseError) {
+            this.setState({ chooseError: true });
+        } else {
+            const tmpupdatedShowing = this.state.updatedShowing;
 
-    this.props.editShowing(
-      this.state.updatedFilm,
-      this.props.location.state.filmId
-    );
-    this.setState({ status: true });
-    this.props.getData();
-    this.props.showShowingsOfThatDay();
-  }
+            //przechodze po wszystkich zaznaczonych miejscach i dodaje je w zajętych miejscach danego seansu
+            const tmpCinemaHall = this.state.cinemaHall;
+            tmpCinemaHall.forEach((row, rowId, rowTab) => {
+                rowTab[rowId].forEach((seat, seatId, seatTab) => {
+                    if (seat.isActive === true) {
+                        tmpupdatedShowing.occupiedSeats = [
+                            ...tmpupdatedShowing.occupiedSeats,
+                            { row: rowId, place: seat.id },
+                        ];
+                    }
+                    return seat;
+                });
+                return row;
+            });
 
-  render() {
-    const { cinemaHall, status, cinemaHallId } = this.state;
-    if (status === true) return <Redirect to="/" />;
-    return (
-      <div className="BuyTicket">
-        <div>
-          <Link to="/">
-            <BsArrowLeftShort />
-          </Link>
-          <h2>Kup bilet / Sala {cinemaHallId}</h2>
-          <div className="cinemaHall">
-            {cinemaHall.map((row, rowNumber) => (
-              <div key={Math.random()} className="row">
-                {row.map((place, placeNumber) => {
-                  return place.id === "X" ? (
-                    <button
-                      onClick={() => this.choosePlace(rowNumber, place)}
-                      key={Math.random()}
-                      className="X"
-                    >
-                      {place.id}
-                    </button>
-                  ) : (
-                    <button
-                      onClick={() => this.choosePlace(rowNumber, place)}
-                      key={Math.random()}
-                      className={place.isActive ? "clicked" : "place"}
-                    >
-                      {place.id}
-                    </button>
-                  );
-                })}
-              </div>
-            ))}
-          </div>
-          <button onClick={() => this.buyTicket()}>Kup bilet</button>
-        </div>
-      </div>
-    );
-  }
+            this.state.updatedFilm.showings.forEach(
+                (showing, showId, showTab) => {
+                    if (showing.showingId === this.state.showingId)
+                        showTab[showId] = tmpupdatedShowing;
+                    return showing;
+                }
+            );
+
+            this.props.editShowing(
+                this.state.updatedFilm,
+                this.props.location.state.filmId
+            );
+            this.setState({ status: true });
+            this.props.getData();
+            this.props.showShowingsOfThatDay();
+        }
+    }
+
+    render() {
+        const { cinemaHall, status, cinemaHallId } = this.state;
+        if (status === true) return <Redirect to="/" />;
+        return (
+            <div className="BuyTicket">
+                <div>
+                    <Link to="/">
+                        <BsArrowLeftShort />
+                    </Link>
+                    <h2>Kup bilet / Sala {cinemaHallId}</h2>
+                    <div className="cinemaHall">
+                        {cinemaHall.map((row, rowNumber) => (
+                            <div key={Math.random()} className="row">
+                                {row.map((place, placeNumber) => {
+                                    return place.id === "X" ? (
+                                        <button
+                                            onClick={() =>
+                                                this.choosePlace(
+                                                    rowNumber,
+                                                    place
+                                                )
+                                            }
+                                            key={Math.random()}
+                                            className="X"
+                                        >
+                                            {place.id}
+                                        </button>
+                                    ) : (
+                                        <button
+                                            onClick={() =>
+                                                this.choosePlace(
+                                                    rowNumber,
+                                                    place
+                                                )
+                                            }
+                                            key={Math.random()}
+                                            className={
+                                                place.isActive
+                                                    ? "clicked"
+                                                    : "place"
+                                            }
+                                        >
+                                            {place.id}
+                                        </button>
+                                    );
+                                })}
+                            </div>
+                        ))}
+                    </div>
+                    <Error
+                        error={this.state.chooseError}
+                        info="Musisz wybrać jakieś miejsce!"
+                    />
+                    <button onClick={() => this.buyTicket()}>Kup bilet</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default BuyTicket;
