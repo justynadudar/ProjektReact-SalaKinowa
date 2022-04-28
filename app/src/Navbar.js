2f54c15a401cac7af5074d47f00ccf9ed2e70b70
Justyna Dudar <justyna.dudar1@gmail.com>
2022-04-28 19:02:23 +0200

    readme

--- app/src/Navbar.js
+++ app/src/Navbar.js
@@ -2,24 +2,24 @@ import "./style/Navbar.css";
 import { NavLink } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <nav className="Navbar">
-            <ul>
-                <li>
-                    <NavLink to="/" exact>
-                        Kasa
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/showings">Seanse</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/films">Filmy</NavLink>
-                </li>
-                <li></li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="Navbar">
+      <ul>
+        <li>
+          <NavLink to="/" activeClassName="active">
+            Kasa
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/showings">Seanse</NavLink>
+        </li>
+        <li>
+          <NavLink to="/films">Filmy</NavLink>
+        </li>
+        <li></li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
